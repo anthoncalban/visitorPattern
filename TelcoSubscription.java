50baee537d99e559440e9d063a58ac55dfbacc7a
Anthon Van Calban <calbananthon@gmail.com>
2026-03-03 11:12:57 +0800

    Delete TelcoSubscription.java

--- TelcoSubscription.java
+++
@@ -1,9 +0,0 @@
-public interface TelcoSubscription {
-    String accept(UsagePromo promo, double price);
-    String accept(UnliCallOffer unliPackage, boolean unliCallText);
-
-    String getTelcoName();
-    double getPromoPrice();
-    int getDataAllowance();
-    boolean getUnliCallText();
-}
