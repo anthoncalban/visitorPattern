69e4c33b016c31abfa2ee08749e16e6b71678da8
Anthon Van Calban <calbananthon@gmail.com>
2026-03-03 11:34:40 +0800

    Add files via upload

---
+++ visitorPattern/Telco.java
@@ -0,0 +1,57 @@
+public class Telco implements TelcoSubscription {
+    private String telcoName;
+    private double promoPrice;
+    private int dataAllowance;
+    private boolean unliCallText;
+
+    public Telco(int dataAllowance, double promoPrice, String telcoName, boolean unliCallText) {
+        this.dataAllowance = dataAllowance;
+        this.promoPrice = promoPrice;
+        this.telcoName = telcoName;
+        this.unliCallText = unliCallText;
+    }
+
+    // Getters
+    public String getTelcoName() {
+        return telcoName;
+    }
+
+    public double getPromoPrice() {
+        return promoPrice;
+    }
+
+    public int getDataAllowance() {
+        return dataAllowance;
+    }
+
+    public boolean getUnliCallText() {
+        return unliCallText;
+    }
+
+    // Setters (optional, if you want mutability)
+    public void setTelcoName(String telcoName) {
+        this.telcoName = telcoName;
+    }
+
+    public void setPromoPrice(double promoPrice) {
+        this.promoPrice = promoPrice;
+    }
+
+    public void setDataAllowance(int dataAllowance) {
+        this.dataAllowance = dataAllowance;
+    }
+
+    public void setUnliCallText(boolean unliCallText) {
+        this.unliCallText = unliCallText;
+    }
+
+    @Override
+    public String accept(UsagePromo promo, double price) {
+        return promo.showAllowance(telcoName, price);
+    }
+
+    @Override
+    public String accept(UniCallsTextOffer unliPackage, boolean unliCallText) {
+        return unliPackage.showUnliCallsTextOffer(telcoName, unliCallText);
+    }
+}
